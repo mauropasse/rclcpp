b5f821c831754bf9690638fca2d6bce23e303548
Chen Lihui <lihui.chen@sony.com>
2021-05-19 18:18:53 +0800

    add a notify guard condition for callback group
    Co-authored-by: William Woodall <william@osrfoundation.org>
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>

--- rclcpp/src/rclcpp/node_interfaces/node_services.cpp
+++ rclcpp/src/rclcpp/node_interfaces/node_services.cpp
@@ -35,11 +35,12 @@ NodeServices::add_service(
       // TODO(jacquelinekay): use custom exception
       throw std::runtime_error("Cannot create service, group not in node.");
     }
-    group->add_service(service_base_ptr);
   } else {
-    node_base_->get_default_callback_group()->add_service(service_base_ptr);
+    group = node_base_->get_default_callback_group();
   }
 
+  group->add_service(service_base_ptr);
+ 
   auto service_intra_process_waitable = service_base_ptr->get_intra_process_waitable();
   if (nullptr != service_intra_process_waitable) {
     // Add to the callback group to be notified about intra-process msgs.
@@ -54,6 +55,7 @@ NodeServices::add_service(
     throw std::runtime_error(
             std::string("failed to notify wait set on service creation: ") + ex.what());
   }
+  group->get_notify_guard_condition()->trigger();
 }
 
 void
@@ -66,11 +68,12 @@ NodeServices::add_client(
       // TODO(jacquelinekay): use custom exception
       throw std::runtime_error("Cannot create client, group not in node.");
     }
-    group->add_client(client_base_ptr);
   } else {
-    node_base_->get_default_callback_group()->add_client(client_base_ptr);
+    group = node_base_->get_default_callback_group();
   }
 
+  group->add_client(client_base_ptr);
+  
   auto client_intra_process_waitable = client_base_ptr->get_intra_process_waitable();
   if (nullptr != client_intra_process_waitable) {
     // Add to the callback group to be notified about intra-process msgs.
@@ -85,6 +88,7 @@ NodeServices::add_client(
     throw std::runtime_error(
             std::string("failed to notify wait set on client creation: ") + ex.what());
   }
+  group->get_notify_guard_condition()->trigger();
 }
 
 std::string
