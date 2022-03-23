57a2dd9f945ac30a842cb3f8840199c54ba50eaf
Mauro Passerino <mpasserino@irobot.com>
2022-03-23 13:10:53 +0000

    Restore include header orig position
    Signed-off-by: Mauro Passerino <mpasserino@irobot.com>

--- rclcpp/src/rclcpp/intra_process_manager.cpp
+++ rclcpp/src/rclcpp/intra_process_manager.cpp
@@ -12,11 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "rclcpp/experimental/intra_process_manager.hpp"
+
 #include <atomic>
 #include <memory>
 #include <mutex>
-
-#include "rclcpp/experimental/intra_process_manager.hpp"
 
 namespace rclcpp
 {
