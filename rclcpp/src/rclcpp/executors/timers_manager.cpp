c752a8b30375e6d62c10d18a668f6e9860d7bfd9
Mauro Passerino <mpasserino@irobot.com>
2022-05-11 13:31:34 +0100

    Check timers thread prio
    Signed-off-by: Mauro Passerino <mpasserino@irobot.com>

--- rclcpp/src/rclcpp/executors/timers_manager.cpp
+++ rclcpp/src/rclcpp/executors/timers_manager.cpp
@@ -52,6 +52,64 @@ void TimersManager::add_timer(rclcpp::TimerBase::SharedPtr timer)
   }
 }
 
+#define HANDLE_ERROR(en, msg) \
+  do {                           \
+    errno = en;                  \
+    perror(msg);                 \
+    exit(EXIT_FAILURE);          \
+  } while (0)
+
+static void display_pthread_attr(pthread_attr_t *attr, char *prefix) {
+  int s, i;
+  size_t v;
+  void *stkaddr;
+  struct sched_param sp;
+
+  s = pthread_attr_getdetachstate(attr, &i);
+  if (s != 0) HANDLE_ERROR(s, "pthread_attr_getdetachstate");
+  printf(
+      "%sDetach state        = %s\n", prefix,
+      (i == PTHREAD_CREATE_DETACHED)
+          ? "PTHREAD_CREATE_DETACHED"
+          : (i == PTHREAD_CREATE_JOINABLE) ? "PTHREAD_CREATE_JOINABLE" : "???");
+
+  s = pthread_attr_getscope(attr, &i);
+  if (s != 0) HANDLE_ERROR(s, "pthread_attr_getscope");
+  printf("%sScope               = %s\n", prefix,
+         (i == PTHREAD_SCOPE_SYSTEM)
+             ? "PTHREAD_SCOPE_SYSTEM"
+             : (i == PTHREAD_SCOPE_PROCESS) ? "PTHREAD_SCOPE_PROCESS" : "???");
+
+  s = pthread_attr_getinheritsched(attr, &i);
+  if (s != 0) HANDLE_ERROR(s, "pthread_attr_getinheritsched");
+  printf(
+      "%sInherit scheduler   = %s\n", prefix,
+      (i == PTHREAD_INHERIT_SCHED)
+          ? "PTHREAD_INHERIT_SCHED"
+          : (i == PTHREAD_EXPLICIT_SCHED) ? "PTHREAD_EXPLICIT_SCHED" : "???");
+
+  s = pthread_attr_getschedpolicy(attr, &i);
+  if (s != 0) HANDLE_ERROR(s, "pthread_attr_getschedpolicy");
+  printf("%sScheduling policy   = %s\n", prefix,
+         (i == SCHED_OTHER)
+             ? "SCHED_OTHER"
+             : (i == SCHED_FIFO) ? "SCHED_FIFO"
+                                 : (i == SCHED_RR) ? "SCHED_RR" : "???");
+
+  s = pthread_attr_getschedparam(attr, &sp);
+  if (s != 0) HANDLE_ERROR(s, "pthread_attr_getschedparam");
+  printf("%sScheduling priority = %d\n", prefix, sp.sched_priority);
+
+  s = pthread_attr_getguardsize(attr, &v);
+  if (s != 0) HANDLE_ERROR(s, "pthread_attr_getguardsize");
+  printf("%sGuard size          = %zu bytes\n", prefix, v);
+
+  s = pthread_attr_getstack(attr, &stkaddr, &v);
+  if (s != 0) HANDLE_ERROR(s, "pthread_attr_getstack");
+  printf("%sStack address       = %p\n", prefix, stkaddr);
+  printf("%sStack size          = %#zx bytes\n", prefix, v);
+}
+
 void TimersManager::start()
 {
   // Make sure that the thread is not already running
@@ -60,6 +118,16 @@ void TimersManager::start()
   }
 
   timers_thread_ = std::thread(&TimersManager::run_timers, this);
+
+  int s;
+  pthread_attr_t gattr;
+  s = pthread_getattr_np(timers_thread_.native_handle(), &gattr);
+  if (s != 0) {
+    HANDLE_ERROR(s, "pthread_getattr_np");
+  }
+
+  printf("TimersManager Thread attributes:\n");
+  display_pthread_attr(&gattr, "\t");
 }
 
 void TimersManager::stop()
