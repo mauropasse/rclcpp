232fa567ce5c9c5acb8dc701261c6dbbe0494dbd
Chen Lihui <lihui.chen@sony.com>
2022-10-27 05:18:21 +0800

    Fix bug that a callback not reached (#1640)
    * Add a test case
    
    a subscriber on a new executor with a callback group triggered to receive a message
    
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>
    
    * fix flaky and not to use spin_some
    
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>
    
    * update comment
    
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>
    
    * update for not using anti-pattern source code
    
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>
    
    * add a notify guard condition for callback group
    
    Co-authored-by: William Woodall <william@osrfoundation.org>
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>
    
    * Notify guard condition of Node not to be used in Executor
    
    it is only for the waitset of GraphListener
    
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>
    
    * put code in the try catch
    
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>
    
    * defer to create guard condition
    
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>
    
    * use context directly for the create function
    
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>
    
    * cpplint
    
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>
    
    * fix that some case might call add_node after shutdown
    
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>
    
    * nitpick and fix some potential bug
    
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>
    
    * add sanity check as some case might not create notify guard condition after shutdown
    
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>
    
    * Cleanup includes.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * remove destroy method
    
    make a callback group can only create one guard condition
    
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>
    
    * remove limitation that guard condition can not be re-created in callback group
    
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>
    
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    Co-authored-by: William Woodall <william@osrfoundation.org>
    Co-authored-by: Chris Lalancette <clalancette@openrobotics.org>

--- rclcpp/src/rclcpp/node_interfaces/node_services.cpp
+++ rclcpp/src/rclcpp/node_interfaces/node_services.cpp
@@ -35,10 +35,10 @@ NodeServices::add_service(
       // TODO(jacquelinekay): use custom exception
       throw std::runtime_error("Cannot create service, group not in node.");
     }
-    group->add_service(service_base_ptr);
   } else {
-    node_base_->get_default_callback_group()->add_service(service_base_ptr);
+    group = node_base_->get_default_callback_group();
   }
+  group->add_service(service_base_ptr);
 
   auto service_intra_process_waitable = service_base_ptr->get_intra_process_waitable();
   if (nullptr != service_intra_process_waitable) {
@@ -50,6 +50,7 @@ NodeServices::add_service(
   auto & node_gc = node_base_->get_notify_guard_condition();
   try {
     node_gc.trigger();
+    group->trigger_notify_guard_condition();
   } catch (const rclcpp::exceptions::RCLError & ex) {
     throw std::runtime_error(
             std::string("failed to notify wait set on service creation: ") + ex.what());
@@ -66,10 +67,10 @@ NodeServices::add_client(
       // TODO(jacquelinekay): use custom exception
       throw std::runtime_error("Cannot create client, group not in node.");
     }
-    group->add_client(client_base_ptr);
   } else {
-    node_base_->get_default_callback_group()->add_client(client_base_ptr);
+    group = node_base_->get_default_callback_group();
   }
+  group->add_client(client_base_ptr);
 
   auto client_intra_process_waitable = client_base_ptr->get_intra_process_waitable();
   if (nullptr != client_intra_process_waitable) {
@@ -81,6 +82,7 @@ NodeServices::add_client(
   auto & node_gc = node_base_->get_notify_guard_condition();
   try {
     node_gc.trigger();
+    group->trigger_notify_guard_condition();
   } catch (const rclcpp::exceptions::RCLError & ex) {
     throw std::runtime_error(
             std::string("failed to notify wait set on client creation: ") + ex.what());
